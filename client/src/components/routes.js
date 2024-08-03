ef9e59ddcf6c97ab39b86a780f5dcdbd138cea4c
John Heger <heger7@gmail.com>
2024-08-03 10:02:19 -0500

    working on routes.js and NavBar

--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -1,22 +1,37 @@
 import App from './App.js'
-
-
-
+import Home from './Home.js'
+import Destinations from './Destinations.js';
+import Itinerary from './Itinerary.js';
+import Calendar from './Calendar.js';
+import Logout from './Logout.js';
 
 
 const routes = [
     {
         path:"/",
-        element: <App />
-        // children: [
-        //     {
-        //         index: true,
-        //         element: <Home />
-        //     },
-        //     {
-                
-        //     }
-        // ]
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                index: "/destinations",
+                element: <Destinations />
+            },
+            {
+                index: "/itineraries",
+                element: <Itinerary />
+            },
+            {
+                index: "/calendar",
+                element: <Calendar />
+            }, 
+            {
+                index: "/logout",
+                element: <Logout />
+            }
+        ]
     }
 ]
 
